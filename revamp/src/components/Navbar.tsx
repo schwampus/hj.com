5318f2c5d76b6f08ae56041dbbf471053e751cf9
Hampus Jerkfelt <hampus.jerkfelt@gmail.com>
2026-01-31 23:51:56 +0100

    tailwinding layout from old css

--- revamp/src/components/Navbar.tsx
+++ revamp/src/components/Navbar.tsx
@@ -2,28 +2,26 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
 	return (
-		<header className="bg-darker p-6">
-			<nav className="flex gap-6">
-				<Link
-					to="/"
-					className="text-salt hover:text-shimmer transition-colors font-body"
-				>
-					Home
-				</Link>
-				<Link
-					to="/about"
-					className="text-salt hover:text-shimmer transition-colors font-body"
-				>
-					About
-				</Link>
-				<Link
-					to="/projects"
-					className="text-salt hover:text-shimmer transition-colors font-body"
-				>
-					Contact
-				</Link>
-			</nav>
-		</header>
+		<nav className="flex justify-center bg-darkest p-2 gap-6">
+			<Link
+				to="/"
+				className="text-sand hover:text-shimmer transition-colors font-body"
+			>
+				Home
+			</Link>
+			<Link
+				to="/about"
+				className="text-sand hover:text-shimmer transition-colors font-body"
+			>
+				About
+			</Link>
+			<Link
+				to="/projects"
+				className="text-sand hover:text-shimmer transition-colors font-body"
+			>
+				Contact
+			</Link>
+		</nav>
 	);
 }
 
