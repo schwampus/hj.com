aef4b6fd64b238a9271f028d287d48dd22b3402a
Hampus Jerkfelt <hampus.jerkfelt@gmail.com>
2026-03-29 21:23:49 +0200

    add analytics

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -13,6 +13,8 @@ import { InitTheme } from '@/providers/Theme/InitTheme'
 import { mergeOpenGraph } from '@/utilities/mergeOpenGraph'
 import { draftMode } from 'next/headers'
 
+import { Analytics } from '@vercel/analytics/next'
+
 import './globals.css'
 import { getServerSideURL } from '@/utilities/getURL'
 
@@ -36,6 +38,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
 
           <Header />
           {children}
+          <Analytics />
           <Footer />
         </Providers>
       </body>
