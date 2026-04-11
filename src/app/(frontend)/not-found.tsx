bccd36615e577dea4f78ca431071372829b5939f
Hampus Jerkfelt <hampus.jerkfelt@gmail.com>
2026-04-11 18:54:32 +0200

    header logo

--- src/app/(frontend)/not-found.tsx
+++ src/app/(frontend)/not-found.tsx
@@ -9,6 +9,7 @@ export default function NotFound() {
       <div className="prose max-w-none">
         <h1 style={{ marginBottom: 0 }}>404</h1>
         <p className="mb-4">This page could not be found.</p>
+        <p className="mb-4">suck to be you</p>
       </div>
       <Button asChild variant="default">
         <Link href="/">Go home</Link>
