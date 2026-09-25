cb7133c7b8a8859baea503b20e0b18e288af7b24
Hampus Jerkfelt <hampus.jerkfelt@gmail.com>
2026-09-25 16:49:22 +0200

    changes

--- src/Header/Nav/index.tsx
+++ src/Header/Nav/index.tsx
@@ -6,7 +6,7 @@ import type { Header as HeaderType } from '@/payload-types'
 
 import { CMSLink } from '@/components/Link'
 import Link from 'next/link'
-import { SearchIcon } from 'lucide-react'
+// import { SearchIcon } from 'lucide-react'
 
 export const HeaderNav: React.FC<{ data: HeaderType }> = ({ data }) => {
   const navItems = data?.navItems || []
@@ -16,10 +16,10 @@ export const HeaderNav: React.FC<{ data: HeaderType }> = ({ data }) => {
       {navItems.map(({ link }, i) => {
         return <CMSLink key={i} {...link} appearance="link" />
       })}
-      <Link href="/search">
+      {/* <Link href="/search">
         <span className="sr-only">Remove Search</span>
         <SearchIcon className="w-5 text-primary" />
-      </Link>
+      </Link> */}
     </nav>
   )
 }
